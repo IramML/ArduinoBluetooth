c54bf2c3473718426e6d145cd3628cb7a0c6c973
IramML <IramMartinezLamas@gmail.com>
2018-06-21 17:43:47 -0500

    Create Android app project

---
+++ ControlApp/app/src/main/java/com/example/iram/controlcart/ControlActivity.java
@@ -0,0 +1,13 @@
+package com.example.iram.controlcart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+    }
+}
